4192d7f0c0feac23b8489562c7f16e518a91b60a
xiongjianqiao <xiongjianqiao@fangpinhui.com>
2015-12-17 18:16:08 +0800

    添加随机函数

--- dev/js/box.js
+++ dev/js/box.js
@@ -1,50 +1,122 @@
 $(function(){
-	var oLis=$("#js_selector").find("li");
 	var randTimer=null;
-
-	//开场动画
+	var cacheData=[];
 	var posArr=[];
-	oLis.each(function(idx,ele){
-		posArr.push({
-			left:$(ele).position().left,
-			top:$(ele).position().top
-		});
-	});
-	oLis.css('position','absolute');
-	rePost();
-	//步骤
-	var step=0,isRunning=false;
-	$("#stepBtn").on('click',function(){
-		var oBtn=$(this);
-		clearInterval(randTimer);
-		if(!isRunning){
-			if(step>2){
-				step=0;
-			}
-			else{
-				step++;
-			}
-			randTimer=setInterval(function(){
-				var rand=_.random(0,oLis.length-1);
-				oLis.removeClass('active');
-				oLis.eq(rand).addClass('active');
-			},40);
-		}
-		else{
-			var oActive=$("#js_selector").find("li.active");
-			oActive.delay(200).fadeOut(600,function(){
-				oActive.remove();
-				oLis=$("#js_selector").find("li");
-			});
-		}
-		isRunning=!isRunning;
+	var step=0;
+	var oBtn=$("#stepBtn");
+	//初始化生成li
+	$.getJSON('./data/list.json',function(data){
+		cacheData=$.extend([],data);
+		createLi(cacheData);
 	});
 
-	function rePost(){
-		oLis.each(function(idx,ele){
+	/*图片hover*/
+	var dBindInfo=_.debounce(bindInfo,500);
+	$("#js_selector").delegate("li","mouseenter",function(){
+		dBindInfo.call($(this));
+	});
+	$("#js_selector").delegate("li","mouseleave",function(){
+		$("#js_selector").find('li').filter('[class=active]').trigger('mouseenter');
+	});
+	/*li点击*/
+	$("#js_selector").delegate("li","click",function(){
+		$(this).addClass('active');
+		$(this).siblings('li').removeClass('active');
+		oBtn.removeAttr("disabled");
+	});
+
+	/*步骤点击*/
+	oBtn.on("click",function(){
+		/*状态机*/
+		switch (step){
+			case 0: //第一步,选择左边角色
+			$("#mask").show();
+			hideLi();
+			break;
+			case 1: //第二步开始进行随机参数
+			randLi();
+			break;
+			case 2: //停止抽取
+			reloadLi();
+			// $("#mask").hide();
+			break;
+		}
+	});
+	/*functions*/
+	function bindInfo(){
+		var oLi=$(this);
+		var headImg=$("#cardOneImg");
+		headImg.parent().addClass('active');
+		headImg.attr('src',oLi.find('img').attr('src')).hide().fadeIn();
+		$("#cardOne").hide().text( oLi.data('name') ).fadeIn();
+	}
+
+	//随机抽取后入栈操作
+	function reloadLi(){
+		clearInterval(randTimer);
+		step=0;
+		oBtn.text(oBtn.data('s'+step)).attr('disabled','disabled');
+		setTimeout(function(){
+			hideLi();//剔除li
+			showLis();//排列li
+			pushInfo();//消息入栈
+		},200);
+	}
+
+	//入栈
+	function pushInfo(){
+		
+	}
+	// 随机li
+	function randLi(){
+		var oLis=$("#js_selector").find('li');
+		var headImg=$("#cardTwoImg");
+		var cardTwo=$("#cardTwo");
+		cardTwo.show();
+		clearInterval(randTimer);
+		randTimer=setInterval(function(){
+			var randIdx=_.random(0,oLis.length-1);
+			oLis.removeClass('active');
+			oLis.eq(randIdx).addClass('active');
+			cardTwo.text(oLis.eq(randIdx).data('name'));
+			headImg.attr('src',oLis.eq(randIdx).find('img').attr('src'));
+		},20);
+		step++;
+		oBtn.text(oBtn.data('s'+step));
+	}
+	//删除active的li
+	function hideLi(){
+		var oLis=$("#js_selector").find('li');
+		oLis.filter('[class=active]').hide(400,function(){
+			$(this).remove();
+			showLis();
+			step++;
+			oBtn.text(oBtn.data('s'+step));
+		});
+	}
+	// 创建li
+	function createLi(data){
+		var strArr=[];
+		$.each(data,function(idx,obj){
+			strArr.push('<li data-name="'+obj.name+'"><img src="'+obj.src+'" alt="" /></li>');
+		});
+		$("#js_selector").html(strArr.join(''));
+		showLis();
+	}
+	// 排列li
+	function showLis(){
+		var oLis=$("#js_selector").find("li");
+		var maxW=$("#js_selector").width();
+		var unitW=oLis.outerWidth(true);
+		var unitH=oLis.outerHeight(true);
+		var cols=maxW/unitW;
+		// 入场动画
+		$.each(oLis,function(idx,ele){
+			var top=Math.floor(idx/cols)*unitH;
+			var left=Math.floor(idx%cols)*unitW;
 			$(ele).css({
-				left:posArr[idx].left,
-				top:posArr[idx].top
+				'top':top,
+				'left':left
 			});
 		});
 	}
